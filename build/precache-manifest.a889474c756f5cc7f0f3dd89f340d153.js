96d53b30192badf7894ed679105d019a8635cc0e
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2019-02-03 19:32:48 +0200

    created new build

---
+++ build/precache-manifest.a889474c756f5cc7f0f3dd89f340d153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/static/media/Chucknorris.39b94c0e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ba3017806d977b27d47",
+    "url": "/static/js/main.7ba30178.chunk.js"
+  },
+  {
+    "revision": "752264ad24facbba4197",
+    "url": "/static/js/1.752264ad.chunk.js"
+  },
+  {
+    "revision": "7ba3017806d977b27d47",
+    "url": "/static/css/main.387a5b6c.chunk.css"
+  },
+  {
+    "revision": "349a2857a380d85adbb829ecf084d957",
+    "url": "/index.html"
+  }
+];
